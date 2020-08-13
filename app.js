bf8a66febc5f2f40b23a6a7fd9ceacf9da5a1aa3
Shakeel-Anjum <shakeel229@gmail.com>
2020-08-13 13:55:32 +1000

    initial working version of calculator

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const calculator = document.querySelector(`.calculator`);
 const keys = calculator.querySelector(`.calculator__keys`);
 const display = calculator.querySelector(`.calculator__display`);
-
 keys.addEventListener(`click`, (e) => {
   if (e.target.matches(`button`)) {
     const key = e.target;
     const action = key.dataset.action;
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
+    let secondValue = displayedNum;
+    const previousKeyType = calculator.dataset.previousKeyType;
+    console.log(previousKeyType);
+    //removing pressed class from the keys if applied
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
 
     if (!action) {
-      if (displayedNum === "0") {
+      calculator.dataset.previousKeyType = "number";
+
+      if (displayedNum === "0" || previousKeyType === "operator") {
         display.textContent = keyContent;
-        console.log("number key!");
+      } else {
+        display.textContent = displayedNum + keyContent;
       }
     }
     if (
@@ -21,20 +30,88 @@ keys.addEventListener(`click`, (e) => {
       action === "multiply" ||
       action === "divide"
     ) {
-      console.log("operator key!");
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      secondValue = displayedNum;
+      if (
+        firstValue &&
+        operator &&
+        previousKeyType !== "operator" &&
+        previousKeyType !== "calculate"
+      ) {
+        const calcValue = calculate(firstValue, operator, secondValue);
+        display.textContent = calcValue;
+        // Update calculated value as firstValue
+        calculator.dataset.firstValue = calcValue;
+      } else {
+        // If there are no calculations, set displayedNum as the firstValue
+        calculator.dataset.firstValue = displayedNum;
+      }
+
+      key.classList.add("is-depressed");
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.operator = action;
     }
 
     if (action === "decimal") {
-      console.log("decimal key!");
+      if (!displayedNum.includes(".")) {
+        display.textContent = displayedNum + ".";
+      } else if (
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
+      ) {
+        display.textContent = "0.";
+      }
+
+      calculator.dataset.previousKeyType = "decimal";
     }
 
     if (action === "clear") {
-      console.log("clear key!");
-    }
+      if (key.textContent === "AC") {
+        calculator.dataset.firstValue = "";
+        calculator.dataset.modValue = "";
+        calculator.dataset.operator = "";
+        calculator.dataset.previousKeyType = "";
+      } else {
+        key.textContent = "AC";
+      }
 
+      display.textContent = 0;
+      calculator.dataset.previousKeyType = "clear";
+    }
+    if (action !== "clear") {
+      const clearButton = calculator.querySelector("[data-action=clear]");
+      clearButton.textContent = "CE";
+    }
     if (action === "calculate") {
-      console.log("equal key!");
+      let firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      let secondValue = displayedNum;
+
+      if (firstValue) {
+        if (previousKeyType === "calculate") {
+          firstValue = displayedNum;
+          secondValue = calculator.dataset.modValue;
+          console.log(
+            firstValue + ":" + secondValue + calculator.dataset.modValue
+          );
+        }
+
+        display.textContent = calculate(firstValue, operator, secondValue);
+      }
+
+      // Set modValue attribute
+      calculator.dataset.modValue = secondValue;
+      calculator.dataset.previousKeyType = "calculate";
     }
   }
 });
-console.log("it works");
+//method to calculate the result of opeartion
+const calculate = (n1, operator, n2) => {
+  const firstNum = parseFloat(n1);
+  const secondNum = parseFloat(n2);
+  if (operator === "add") return firstNum + secondNum;
+  if (operator === "subtract") return firstNum - secondNum;
+  if (operator === "multiply") return firstNum * secondNum;
+  if (operator === "divide") return firstNum / secondNum;
+};
